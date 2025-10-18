e5ab210dda0abcc984c6834bf5b3327f26610ca8
JaxChoong <choongjax@gmail.com>
2025-10-18 17:33:30 +0800

    feat: now detects substring in each word

--- substring.rb
+++ substring.rb
@@ -3,7 +3,9 @@ def substrings(string,dictionary)
   words = string.gsub(/[^a-zA-Z\s]/,' ').downcase.split(" ")
   found_words = Hash.new(0)
   words.each do |word|
-    dictionary.include?(word) ? (found_words[word] += 1) : nil
+    dictionary.each do |entry|
+      word.include?(entry) ? (found_words[entry] += 1) : nil
+    end
   end
   p found_words
 end
