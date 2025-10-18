a69f72d57392c887727ab6c3c5c8527bf6662704
JaxChoong <choongjax@gmail.com>
2025-10-18 17:30:35 +0800

    feat: now detects each word in dict that matches string

--- substring.rb
+++ substring.rb
@@ -1,6 +1,11 @@
 def substrings(string,dictionary)
   # split words into array, the gsub replaces punctuation with space
-  words = string.gsub(/[^a-zA-Z\s]/,' ').split(" ")
-
-  p words
+  words = string.gsub(/[^a-zA-Z\s]/,' ').downcase.split(" ")
+  found_words = Hash.new(0)
+  words.each do |word|
+    dictionary.include?(word) ? (found_words[word] += 1) : nil
+  end
+  p found_words
 end
+
+substrings("Howdy partner, sit down! How's it going?", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
